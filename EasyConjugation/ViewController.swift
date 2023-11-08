5df76c30a2c0ccd02858a4a35f253ea3d46bc403
Laura Silva <laura@git.com>
2023-11-08 15:20:52 -0500

    first commit

---
+++ EasyConjugation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EasyConjugation
+//
+//  Created by Laura Silva on 2023-11-08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
